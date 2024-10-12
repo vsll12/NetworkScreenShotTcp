bbfcbc7355fd6ea9390a31508efe442a2e527cd4
unknown <bekirovvusal53@gmail.com>
2024-10-12 17:02:31 +0300

    Screen Shot Task TCP

---
+++ ScreenShotNetWork/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+//client ekrani 10 saniyeden bir screenshot edib servere gonderir.
+//server client uchun folder yaradib, onun ichirisine download edir.
+
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var ipAddress = IPAddress.Parse("192.168.1.82");
+
+        var port = 54751;
+
+        var endPoint = new IPEndPoint(ipAddress, port);
+
+        var client = new TcpClient();
+
+        int counter = 0;
+
+        client.Connect(endPoint);
+
+        if (client.Connected)
+        {
+
+            while (true)
+            {
+                var networkStream = client.GetStream();
+
+                var path = GetScreen(counter);
+
+                var bytes = new byte[150000];
+
+                var memoryStream = new MemoryStream();
+
+                int len = 0;
+
+
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+
+                    len = fs.Read(bytes, 0, bytes.Length);
+                    
+                    networkStream.Write(bytes, 0, len);
+                    
+                }
+
+                Console.WriteLine($"Process{counter + 1} finished successfully");
+
+                counter++;
+
+                Thread.Sleep(10000);
+
+            }
+        }
+
+        Console.ReadKey();
+
+    }
+    public static string GetScreen(int counter)
+    {
+
+        int screenWidth = 1920;
+        int screenHeight = 1080;
+
+        using (Bitmap screenshot = new Bitmap(screenWidth, screenHeight))
+        {
+
+            using (Graphics g = Graphics.FromImage(screenshot))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, new Size(screenWidth, screenHeight));
+            }
+
+            var path = $"C:\\Users\\DELL\\Desktop\\Screenshots\\screenshot{counter++}.png";
+
+            screenshot.Save(path, ImageFormat.Png);
+
+            return path;
+        }
+    }
+}
---
+++ ScreenshotServer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.Metrics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var ipAddress = IPAddress.Parse("192.168.1.82");
+
+        var port = 54751;
+
+        var endPoint = new IPEndPoint(ipAddress, port);
+
+        int counter = 0;
+
+        var server = new TcpListener(endPoint);
+
+        server.Start();
+
+        while (true)
+        {
+
+            var client = server.AcceptTcpClient();
+
+            var dInfo = Directory.CreateDirectory($"C:\\Users\\DELL\\Desktop\\ServerSS\\{client.Client.ToString()}");
+
+            _ = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    Console.Clear();
+
+                    var networkStream = client.GetStream();
+
+                    byte[] buffer = new byte[150000];
+
+                    int bytesRead = 0;
+
+                    Console.WriteLine("Creating a new file for new screenshot...");
+
+                    await Task.Delay(2000); 
+
+                    using (var fs = new FileStream($"{dInfo.FullName}\\screens{counter++}.png", FileMode.Create, FileAccess.Write))
+                    {
+
+                            bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                        
+                             fs.Write(buffer, 0, bytesRead);
+                        
+                    }
+                    Console.WriteLine($"{counter}.File received");
+                    await Task.Delay(1000);
+                }
+            });
+
+        }
+    }
+}
